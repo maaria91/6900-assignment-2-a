36e4565c0b0f00d3f9b27547a7f7e6c61cde754e
Maaria <Maaria@Maarias-MacBook-Air.local>
2016-03-03 13:52:31 -0500

    age is only a number

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function dataLoaded(err,rows) {
 
 //First: setting up the crossfilter
     var crossFilter = crossfilter(rows);
-
+console. log (rows);
 
 //#1: total number of trips in 2012
     var start2012 = new Date ('January 01, 2012 00:00:00'),
@@ -19,7 +19,7 @@ function dataLoaded(err,rows) {
     var tripsByYear = crossFilter.dimension(function(rows) { return rows.startTime; });
     tripsIn2012 = tripsByYear.filter([start2012, end2012]).top(Infinity); // selects dates that are between the two variables we selected right on top
 
-    //console.log("trips in 2012", tripsIn2012.length);
+    console.log("trips in 2012", tripsIn2012.length);
 
 //#2 trips in 2012 that are taken by males
     var tripsByGender = crossFilter.dimension(function(rows){ return rows.sex; });
@@ -29,19 +29,20 @@ function dataLoaded(err,rows) {
     var tripsByReg = crossFilter.dimension(function(rows){ return rows.regulated});
     TripsGuysReg = tripsByReg.filter("Registered").top(Infinity);
 
-    //console.log("guys in 2012 that are reg:",TripsGuysReg.length);
+    console.log("guys in 2012 that are reg:",TripsGuysReg.length);
 
 //total number of trips in 2012, by all users (male, female, or unknown)
     var tripsByStation = crossFilter.dimension(function(rows){return rows.startStation});
         tripsByNEUStation = tripsByStation.filter("5").top(Infinity);
-        //console.log("Northeastern Trips:", tripsByNEUStation.length);
+
+    console.log("Northeastern station Trips:", tripsByNEUStation.length);
 
 
 //top 50 trips, in all time, by all users, regardless of starting point, in terms of trip duration.
     var tripsByDuration = crossFilter.dimension(function(rows){return rows.duration});
     tripsTop50 = tripsByDuration.top(50);
 
-    //console.log("top 50 trips from NEU station taken by guys that are reg:", tripsTop50);
+    console.log("top 50 trips from NEU station taken by guys that are reg:", tripsTop50);
 
 
 //clear all filters
@@ -52,16 +53,16 @@ function dataLoaded(err,rows) {
 
 
 //grouping by 10-year age buckets:
-    var tripsByAge = crossFilter.dimension(function(rows){return rows.age});
+    var tripsByAge = crossFilter.dimension(function(rows){return rows.userAge});
     tripsByAgeGroups = tripsByAge.group(function(rows){return Math.floor(rows/10)});
-
-    var group = tripsByAgeGroups.all();
-    console.log("Trips separated by age group by decade ", group);
+    
+    //var grouping = tripsByAgeGroups.all();
+    //console.log("Trips separated by age group by 10-year age buckets:", grouping);
 
 
 }
-function parse(d){
-    if(+d.duration<0) return;
+function parse(d) {
+    if (+d.duration < 0) return;
 
     return {
         duration: +d.duration,
@@ -71,10 +72,13 @@ function parse(d){
         regulated: d.subsc_type,//must add this to use above
         sex: d.gender,//must add this to use above
         endStation: d.end_statn,
-        age: 2016-(+d.birth_date)
+        userAge: d.birth_date?parseDate(d.start_date).getFullYear()-d.birth_date:0
+
+        //age: parseAge(d.end_date, d.birth_date)
+        //age: 2016-(+d.birth_date)
+
     }
 }
-
 function parseDate(date){
     var day = date.split(' ')[0].split('/'),
         time = date.split(' ')[1].split(':');
@@ -82,3 +86,8 @@ function parseDate(date){
     return new Date(+day[2],+day[0]-1, +day[1], +time[0], +time[1]);
 }
 
+//function parseAge(date, birthdate){
+//    var day = date.split(' ')[0].split('/');
+//return (day[2] - birthdate)
+//}
+//
